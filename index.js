8c3cc4d9d91f19f0cf854914f3913bab515b1781
Aman Verma <verma1090aman@gmail.com>
2021-05-21 16:31:02 +0530

    Added support for url-encoded

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 //Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(helmet());
 app.use(cors());
 app.use(compression());
